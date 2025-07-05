8bd8b4da9162ca933e9aa6c31eeb90a5d4a8b360
Louis Thevenet <louis.tvnt@gmail.com>
2025-07-05 11:50:41 +0200

    chore: use convenient method in fill_template example

--- examples/fill_template.rs
+++ examples/fill_template.rs
@@ -1,8 +1,10 @@
-use open_editor::EditorCallBuilder;
+use open_editor::edit_string;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let template = "Hello, {name}!\nWelcome to {place}.";
-    let filled_template = EditorCallBuilder::new().edit_string(template)?;
+    // Also works:
+    // let filled_template = EditorCallBuilder::new().edit_string(template)?;
+    let filled_template = edit_string(template)?;
     println!("Filled Template:\n{filled_template}");
     Ok(())
 }
